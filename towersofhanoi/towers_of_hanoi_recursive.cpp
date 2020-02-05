9cf4cd42f7861cd6ccc1e3b84c73aabd88afc2e7
Sanchit987 <kherasanchit1@gmail.com>
2020-02-05 14:27:10 +0530

    Tower of hanoi

--- towersofhanoi/towers_of_hanoi_recursive.cpp
+++ towersofhanoi/towers_of_hanoi_recursive.cpp
@@ -1,3 +1,25 @@
 #include<iostream>
 using namespace std;
 
+void tower(int n,char beg,char aux,char end)
+{
+	if(n==1)
+	{
+		cout<<"move disk "<<n<<" from "<<beg<<" to "<<end<<endl;
+	}
+	else
+	{
+		tower(n-1,beg,end,aux);
+		cout<<"move disk "<<n<<" from "<<beg<<" to "<<end<<endl;
+		tower(n-1,aux,beg,end);
+	}
+}
+
+int main()
+{
+	cout<<"Enter Number Of disks : ";
+	int n;
+	cin>>n;
+	tower(n,'A','B','C');
+	return 0;
+}
