6fbdbd5c46d808485ab8eff7c54b50ed62fad61e
Sanchit987 <kherasanchit1@gmail.com>
2020-04-29 12:24:12 +0530

    Sorting completed

--- sort/sort.cpp
+++ sort/sort.cpp
@@ -11,6 +11,8 @@ void quick_sort(int (&arr)[100],int low,int high);
 int quick(int (&arr)[100],int low,int high);
 void swap(int *a,int *b);
 
+void radixsort(int (&arr)[100], int n);
+
 void merge_sort(int (&arr)[100],int low,int high);
 void merge(int (&arr)[100],int low,int mid,int high);
 
@@ -61,6 +63,10 @@ label1:
 			merge_sort(arr,0,n);
 			display(arr,n);
 			break;
+		case 6:
+                        radixsort(arr,n);
+			display(arr,n);
+                        break;
 		case 7:
 			goto label2;
 			break;
@@ -162,6 +168,47 @@ void swap(int &a,int &b)
 	a = b;
 	b = temp;
 }
+
+int getMax(int arr[], int n)
+{
+    int mx = arr[0];
+    for (int i = 1; i < n; i++)
+        if (arr[i] > mx)
+            mx = arr[i];
+    return mx;
+}
+
+
+void countSort(int arr[], int n, int exp)
+{
+    int output[n];
+    int i, count[10] = {0};
+
+
+    for (i = 0; i < n; i++)
+        count[ (arr[i]/exp)%10 ]++;
+
+      for (i = 1; i < 10; i++)
+        count[i] += count[i - 1];
+
+      for (i = n - 1; i >= 0; i--)
+    {
+        output[count[ (arr[i]/exp)%10 ] - 1] = arr[i];
+        count[ (arr[i]/exp)%10 ]--;
+    }
+
+    for (i = 0; i < n; i++)
+        arr[i] = output[i];
+}
+
+void radixsort(int (&arr)[100], int n)
+{
+
+    int m = getMax(arr, n);
+    for (int exp = 1; m/exp > 0; exp *= 10)
+        countSort(arr, n, exp);
+}
+
 
 void merge_sort(int (&arr)[100],int low,int high)
 {
